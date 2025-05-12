23a6672903b0b1527cd8aa50bec83c6b531bf711
SargamPuram <sargampuram3@gmail.com>
2025-05-12 12:20:01 +0530

    Added api url

--- frontend/lib/imports.ts
+++ frontend/lib/imports.ts
@@ -1,3 +1,6 @@
-export const ROOT_URL = "http://localhost:3001";
+// export const ROOT_URL = "http://localhost:3001";
 
-export const API_URL = `${ROOT_URL}/api`;
+// export const API_URL = `${ROOT_URL}/api`;
+export const ROOT_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
+
